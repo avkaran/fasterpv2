5f846c4c124af2be5082825621a974bbb6890962
avkaran <111076158+avkaran@users.noreply.github.com>
2023-02-28 18:49:55 +0530

    update api coder

--- src/pages/admin/business/mywatch/models/devTools.js
+++ src/pages/admin/business/mywatch/models/devTools.js
@@ -85,83 +85,83 @@ const getAllTables = async (project) => {
 
     });
 }
-const generateEditQuery=(selColumns)=>{
-    var errors=[];
-    var tables=[];
-    var primaryTable={table:'',objectName:''};
-    if(selColumns.length>0){
-        primaryTable.table=selColumns[0].tableName;
-        primaryTable.objectName=selColumns[0].objectName;
+const generateEditQuery = (selColumns) => {
+    var errors = [];
+    var tables = [];
+    var primaryTable = { table: '', objectName: '' };
+    if (selColumns.length > 0) {
+        primaryTable.table = selColumns[0].tableName;
+        primaryTable.objectName = selColumns[0].objectName;
     }
-    
-    
-    var uniqueTables = [...new Set(selColumns.map(item => item.tableName+"."+item.objectName))];
-    uniqueTables.forEach(item=>{
-        var tableSplit=item.split(".");
-        if(tableSplit[1]){
-            var columns=selColumns.filter(column=>column.tableName===tableSplit[0]);
+
+
+    var uniqueTables = [...new Set(selColumns.map(item => item.tableName + "." + item.objectName))];
+    uniqueTables.forEach(item => {
+        var tableSplit = item.split(".");
+        if (tableSplit[1]) {
+            var columns = selColumns.filter(column => column.tableName === tableSplit[0]);
             tables.push({
-                tableName:tableSplit[0],
-                objectName:tableSplit[1],
-                columns:columns
+                tableName: tableSplit[0],
+                objectName: tableSplit[1],
+                columns: columns
             })
         }
-        else    
-            errors.push("Table :" + tableSplit[0] +" has no object name in comment");
+        else
+            errors.push("Table :" + tableSplit[0] + " has no object name in comment");
 
     })
-    var fromClauses=[];
-    tables.forEach(table=>{
-       fromClauses.push(table.tableName+" "+ table.objectName)
+    var fromClauses = [];
+    tables.forEach(table => {
+        fromClauses.push(table.tableName + " " + table.objectName)
     })
-    var selectQueryColumns=[];
-    selColumns.forEach(col=>{
-        selectQueryColumns.push(col.objectName+"."+col.columnName)
+    var selectQueryColumns = [];
+    selColumns.forEach(col => {
+        selectQueryColumns.push(col.objectName + "." + col.columnName)
     })
-    var selectQueryFromTables=[];
-    tables.forEach(table=>{
-        selectQueryFromTables.push(table.tableName+" "+ table.objectName)
+    var selectQueryFromTables = [];
+    tables.forEach(table => {
+        selectQueryFromTables.push(table.tableName + " " + table.objectName)
     })
-    if(errors.length>0)
- //   setErrorList([...errorList,...errors])
+    if (errors.length > 0)
+        //   setErrorList([...errorList,...errors])
 
-    var finalQuery=`"SELECT ` + selectQueryColumns.join(",") + ` from ` + selectQueryFromTables.join(",") + ` where `+ primaryTable.objectName+`.status=1 and ` + primaryTable.objectName+`.id="+id`
+        var finalQuery = `"SELECT ` + selectQueryColumns.join(",") + ` from ` + selectQueryFromTables.join(",") + ` where ` + primaryTable.objectName + `.status=1 and ` + primaryTable.objectName + `.id="+id`
     return finalQuery;
 
 }
-const getEditValues=(selColumns,formVar)=>{
-    var tables=[];
+const getEditValues = (selColumns, formVar) => {
+    var tables = [];
     var uniqueTables = [...new Set(selColumns.map(item => item.tableName))];
-    uniqueTables.forEach(item=>{
-            var columns=selColumns.filter(column=>column.tableName===item);
-            tables.push({
-                tableName:item,
-                columns:columns
-            })
+    uniqueTables.forEach(item => {
+        var columns = selColumns.filter(column => column.tableName === item);
+        tables.push({
+            tableName: item,
+            columns: columns
+        })
     })
-    console.log('test',uniqueTables,selColumns)
-    var resultStr=``;
-    tables.forEach(table=>{
-        var tmpStr=``;
-        
-        table.columns.forEach(col=>{
-            tmpStr=tmpStr+`
-            `+col.columnName+`:mydata.`+col.columnName+`,
+    console.log('test', uniqueTables, selColumns)
+    var resultStr = ``;
+    tables.forEach(table => {
+        var tmpStr = ``;
+
+        table.columns.forEach(col => {
+            tmpStr = tmpStr + `
+            `+ col.columnName + `:mydata.` + col.columnName + `,
             `;
         })
-        resultStr=resultStr+`
-        `+table.tableName+`:{`+tmpStr+`}`
-     })
-     
-    return formVar+`.setFieldsValue({
-    `+resultStr+`
+        resultStr = resultStr + `
+        `+ table.tableName + `:{` + tmpStr + `}`
+    })
+
+    return formVar + `.setFieldsValue({
+    `+ resultStr + `
     });`;
 }
-const getReactCode = (allTableObjects, selColumns,moduleType,moduleName='test') => {
+const getReactCode = (allTableObjects, selColumns, moduleType, moduleName = 'test') => {
     var FormItemsCode = '';
     var FormViewItemsCode = '';
     let cnt = 0;
-    var selColumnObjects=[];
+    var selColumnObjects = [];
     selColumns.forEach(selColumn => {
         // console.log('test2',selColumn.split(".")[0])
         var curTable = allTableObjects.find(item => item.tableName === selColumn.split(".")[0])
@@ -171,7 +171,7 @@ const getReactCode = (allTableObjects, selColumns,moduleType,moduleName='test') 
         var columnText = capitalizeFirst(column.columnName.replaceAll("_", " "));
         var columnVar = capitalizeFirst(column.columnName.replaceAll("_", " ")).replaceAll(" ", "");
         columnVar = columnVar.charAt(0).toLowerCase() + columnVar.slice(1);
-        
+
         if (column.inputConstraint.template)
             template = column.inputConstraint.template;
         template = template.replace("defaultValue={defaultValue}", "");
@@ -182,7 +182,7 @@ const getReactCode = (allTableObjects, selColumns,moduleType,moduleName='test') 
         fItemTemplate = fItemTemplate.replace("{label}", columnText).replace("{name}", "{['" + column.tableName + "','" + column.columnName + "']}").replace("{placeHolder}", columnText);
         fItemTemplate = fItemTemplate.replace("{inputRules}", `rules={[{ required: true, message: 'Please Enter ` + columnText + `' }]}`);
 
-        var isTwoColumnForm=true;
+        var isTwoColumnForm = true;
         if (isTwoColumnForm) {
 
             var fiFinal = fItemTemplate.replace("{inputTemplate}", template);
@@ -233,56 +233,56 @@ const getReactCode = (allTableObjects, selColumns,moduleType,moduleName='test') 
         cnt = cnt + 1;
     })
 
-    var formVar=moduleType.replace("-","")+"Form"+ moduleName.replace(" ","");
-         var formCode=FormTemplate.replaceAll("{formVar}",formVar).replace("{formItems}",FormItemsCode);
+    var formVar = moduleType.replace("-", "") + "Form" + moduleName.replace(" ", "");
+    var formCode = FormTemplate.replaceAll("{formVar}", formVar).replace("{formItems}", FormItemsCode);
 
-         var pageCode='';
-         if(moduleType==="add-edit"){
-            pageCode=AddEditModuleTemplate.replaceAll("{pageName}","AddEdit"+ capitalizeFirst(moduleName).replace(" ",""));
-            pageCode=pageCode.replaceAll("{formVar}",formVar);
-            var primaryTable="";
-            if(selColumnObjects.length>0)
-               primaryTable=selColumnObjects[0].tableName;
-   
-            pageCode=pageCode.replaceAll("{primaryTable}",primaryTable);
-            pageCode=pageCode.replace("{editQuery}",generateEditQuery(selColumnObjects));
-            pageCode=pageCode.replace("{setEditFieldsValues}",getEditValues(selColumnObjects,formVar));
-            pageCode=pageCode.replace("{form}",formCode)
+    var pageCode = '';
+    if (moduleType === "add-edit") {
+        pageCode = AddEditModuleTemplate.replaceAll("{pageName}", "AddEdit" + capitalizeFirst(moduleName).replace(" ", ""));
+        pageCode = pageCode.replaceAll("{formVar}", formVar);
+        var primaryTable = "";
+        if (selColumnObjects.length > 0)
+            primaryTable = selColumnObjects[0].tableName;
 
-         }else if(moduleType==="view"){
-            pageCode=viewModuleTemplate.replaceAll("{pageName}","View"+ capitalizeFirst(moduleName));
-            pageCode=pageCode.replace("{viewQuery}",generateEditQuery(selColumnObjects));
-           // pageCode=pageCode.replace("{setEditFieldsValues}",getEditValues(selColumns,formVar));
-            pageCode=pageCode.replace("{viewItems}",FormViewItemsCode)
-         }
-        
-        // generateEditQuery(selColumns);
-         return(pageCode)
+        pageCode = pageCode.replaceAll("{primaryTable}", primaryTable);
+        pageCode = pageCode.replace("{editQuery}", generateEditQuery(selColumnObjects));
+        pageCode = pageCode.replace("{setEditFieldsValues}", getEditValues(selColumnObjects, formVar));
+        pageCode = pageCode.replace("{form}", formCode)
+
+    } else if (moduleType === "view") {
+        pageCode = viewModuleTemplate.replaceAll("{pageName}", "View" + capitalizeFirst(moduleName));
+        pageCode = pageCode.replace("{viewQuery}", generateEditQuery(selColumnObjects));
+        // pageCode=pageCode.replace("{setEditFieldsValues}",getEditValues(selColumns,formVar));
+        pageCode = pageCode.replace("{viewItems}", FormViewItemsCode)
+    }
+
+    // generateEditQuery(selColumns);
+    return (pageCode)
 }
-const getPHPApiModalFunctionCode=(functionType,tableObject,functionSuffixName='')=>{
-    var resultStr='';
-    if(functionType=='add'){
-        var addValidationItemsCode='';
-        var addPdoItemsCode='';
-        var addPdoIfItemsCode='';
-        tableObject.columns.forEach(column=>{
-            if(!column.isNullable && column.columnName!='id' && !column.columnDefault){
-                addValidationItemsCode=addValidationItemsCode+`
+const getPHPApiModalFunctionCode = (functionType, tableObject, functionSuffixName = '') => {
+    var resultStr = '';
+    if (functionType === 'add') {
+        var addValidationItemsCode = '';
+        var addPdoItemsCode = '';
+        var addPdoIfItemsCode = '';
+        tableObject.columns.forEach(column => {
+            if (!column.isNullable && column.columnName != 'id' && !column.columnDefault) {
+                addValidationItemsCode = addValidationItemsCode + `
                 '${column.columnName}' =>	'required',`;
-                 addPdoItemsCode=addPdoItemsCode+`
+                addPdoItemsCode = addPdoItemsCode + `
                  '${column.columnName}' =>	$this->request->get('${column.columnName}'),
                 `;
-            }else{
+            } else {
                 //use other items in the if condition.
-                if(column.columnName!='id'){
-                    addPdoIfItemsCode=addPdoIfItemsCode+`
+                if (column.columnName != 'id') {
+                    addPdoIfItemsCode = addPdoIfItemsCode + `
                     if ($this->request->get('${column.columnName}'))
                         $pdata['${column.columnName}'] = $this->request->get('${column.columnName}');`;
                 }
             }
         })
-        var addModelCode=`
-        public function xpostAdd`+capitalizeFirst(functionSuffixName).replaceAll(' ','')+`()
+        var addModelCode = `
+        public function xpostAdd`+ capitalizeFirst(functionSuffixName).replaceAll(' ', '') + `()
         {
             try {
     
@@ -313,22 +313,22 @@ const getPHPApiModalFunctionCode=(functionType,tableObject,functionSuffixName=''
                 $this->jsonOutput(['status' => '0', 'message' => $ex->getMessage()]);
             }
         }`;
-        resultStr=addModelCode;
-    }else if(functionType='update'){
+        resultStr = addModelCode;
+    } else if (functionType === 'update') {
 
-            var updatePdoIfItemsCode='';
-            tableObject.columns.forEach(column=>{
+        var updatePdoIfItemsCode = '';
+        tableObject.columns.forEach(column => {
 
-                    //use other items in the if condition.
-                    if(column.columnName!='id'){
-                        updatePdoIfItemsCode=updatePdoIfItemsCode+`
+            //use other items in the if condition.
+            if (column.columnName != 'id') {
+                updatePdoIfItemsCode = updatePdoIfItemsCode + `
                         if ($this->request->get('${column.columnName}'))
                             $pdata['${column.columnName}'] = $this->request->get('${column.columnName}');`;
-                    }
-                
-            })
-            var updateModalCode=`
-            public function xpostUpdate`+capitalizeFirst(functionSuffixName).replaceAll(' ','')+`()
+            }
+
+        })
+        var updateModalCode = `
+            public function xpostUpdate`+ capitalizeFirst(functionSuffixName).replaceAll(' ', '') + `()
             {
                 try {
         
@@ -358,11 +358,125 @@ const getPHPApiModalFunctionCode=(functionType,tableObject,functionSuffixName=''
                     $this->jsonOutput(['status' => '0', 'message' => $ex->getMessage()]);
                 }
             }`;
-            resultStr=updateModalCode;
+        resultStr = updateModalCode;
+
+    } else if (functionType === 'total-records') {
+
+        var tableObjectForQuery = 'a';
+        if (tableObject.tableObject && tableObject.tableObject !== 'N/A')
+            tableObjectForQuery = tableObject.tableObject;
+        var ifWhereClauseItemsCode = '';
+        tableObject.columns.forEach(column => {
+
+            //use other items in the if condition.
+            if (column.columnName != 'id') {
+                ifWhereClauseItemsCode = ifWhereClauseItemsCode + `
+                        if ($this->request->get('${column.columnName}'))
+                        $query .= " AND ${tableObjectForQuery}.${column.columnName}='{$this->request->get('${column.columnName}')}'";`;
+            }
+
+        })
+        var totalRecordsModalCode = `
+            public function xpostTotalRecords`+ capitalizeFirst(functionSuffixName).replaceAll(' ', '') + `()
+            {
+                try {
+                    $post = $_REQUEST;
+                    $validator = new Validator;
+                    /*
+                    $validation = $validator->make($post, [
+                       
+                    ]);
+                    $validation->validate();
+                    if ($validation->fails()) $this->jsonOutput(['status' =>  0, 'message' =>  $validation->errors()->all()]);
+                    */
         
+                    $query = "select count(*) as count from ${tableObject.tableName} ${tableObjectForQuery} where ${tableObjectForQuery}.status=1 ";
+        
+                    ${ifWhereClauseItemsCode}
+        
+                    $sql = $this->db->query($query)->fetchAll();
+        
+                    if (sizeof($sql) < 1) throw new \Exception('No data');
+        
+                    $this->jsonOutput([
+                        'status'		=>	'1',
+                        'message'		=>	'Success',
+                        'data'			=>	$sql[0]->count,
+                    ]);
+                } catch (\Exception $ex) {
+                    $this->jsonOutput(['status' => '0', 'message' => $ex->getMessage()]);
+                }
+            }`;
+        resultStr = updateModalCode;
+
+    } else if (functionType === 'list') {
+
+        let tableObjectForQuery = 'a';
+        if (tableObject.tableObject && tableObject.tableObject !== 'N/A')
+            tableObjectForQuery = tableObject.tableObject;
+        let ifWhereClauseItemsCode = '';
+        tableObject.columns.forEach(column => {
+
+            //use other items in the if condition.
+            if (column.columnName != 'id') {
+                ifWhereClauseItemsCode = ifWhereClauseItemsCode + `
+                        if ($this->request->get('${column.columnName}'))
+                        $query .= " AND ${tableObjectForQuery}.${column.columnName}='{$this->request->get('${column.columnName}')}'";`;
+            }
+
+        })
+        var listModalCode = `
+            public function xpostList`+ capitalizeFirst(functionSuffixName).replaceAll(' ', '') + `()
+            {
+                try {
+                    $post = $_REQUEST;
+                    $validator = new Validator;
+                    /*
+                    $validation = $validator->make($post, [
+                        'type'        	  	=>	'required',
+                    ]);
+                    $validation->validate();
+                    if ($validation->fails()) $this->jsonOutput(['status' =>  0, 'message' =>  $validation->errors()->all()]);
+                    */
+        
+                    $query = "select ${tableObjectForQuery}.* from ${tableObject.tableName} ${tableObjectForQuery} where ${tableObjectForQuery}.status=1";
+        
+                    ${ifWhereClauseItemsCode}
+
+                    
+
+                    if ($this->request->get('order_by_array') && is_array( json_decode($this->request->get('order_by_array')) )){
+                        $orderbyClauses=array();
+                        $orderByArray=json_decode($this->request->get('order_by_array'));
+                        foreach($orderByArray as  $item){
+                            $orderbyClauses[]=$item->field.' '.$item->order_by;
+                        }
+                        $query .= "  ORDER BY ". implode($orderbyClauses,',');
+                    }
+                    
+                    
+        
+                    if ($this->request->get('length'))
+                        $query .= " LIMIT {$this->request->get('start')},{$this->request->get('length')}";
+        
+                    $sql = $this->db->query($query)->fetchAll();
+        
+                    if (sizeof($sql) < 1) throw new \Exception('No data');
+        
+                    $this->jsonOutput([
+                        'status'		=>	'1',
+                        'message'		=>	'Success',
+                        'data'			=>	$sql,
+                    ]);
+                } catch (\Exception $ex) {
+                    $this->jsonOutput(['status' => '0', 'message' => $ex->getMessage()]);
+                }
+            }`;
+        resultStr = listModalCode;
+
     }
     return resultStr;
-   
+
 }
 export {
     getAllTables,
